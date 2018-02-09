563e45f4f14b4a6daf6de45330300245f02129d2
CXDsone <34830558+CXDsone@users.noreply.github.com>
2018-02-09 15:28:07 +0800

    Operation

---
+++ Test/app/src/main/java/com/example/smartcabinet/OperationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.smartcabinet
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class OperationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_operation)
+    }
+}
