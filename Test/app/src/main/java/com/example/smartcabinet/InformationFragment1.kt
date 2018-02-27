79ee05445c1cd93dd11351da80de6099b49cb38e
CXDsone <34830558+CXDsone@users.noreply.github.com>
2018-02-27 10:11:04 +0800

    子操作界面

--- Test/app/src/main/java/com/example/smartcabinet/InformationFragment1.kt
+++ Test/app/src/main/java/com/example/smartcabinet/InformationFragment1.kt
@@ -1,6 +1,5 @@
 package com.example.smartcabinet
 
-
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -8,16 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 
 
+
 /**
  * A simple [Fragment] subclass.
  */
-class SubOperationFragment : Fragment() {
+class InformationFragment1 : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_sub_operation, container, false)
+        return inflater!!.inflate(R.layout.fragment_information1, container, false)
     }
 
 }// Required empty public constructor
---
+++ Test/app/src/main/java/com/example/smartcabinet/SubOperationActivity.kt
@@ -0,0 +1,14 @@
+package com.example.smartcabinet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sub_operation.*
+
+class SubOperationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub_operation)
+    }
+
+}
